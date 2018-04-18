698a24c92cd49c5947ee6449073918ad9e55dfab
Logan Phillips <LoganPhillips@Logans-MacBook-Pro-2.local>
2018-04-18 12:59:50 -0400

    changed stuff, added LogEntry class

--- src/Leader.java
+++ src/Leader.java
@@ -2,17 +2,7 @@ import com.reber.raft.AppendEntriesProtos;
 
 public class Leader extends Node {
 
-    /*Upon election: send initial empty AppendEntries RPCs (heartbeat) to each server; repeat
-    during idle periods to prevent election timeouts
-
-    If command receieved from client: append entry to local log, respond after entry applied to
-    state machine
-
-    If last log index >= nextIndex for a follower: send
-    AppendEntries RPC with log entries starting at nextIndex
-        If successful: update nextIndex and matchIndex for follower
-
-    */
+    
 
     //if leaderCommit > follower commit, follower can update
     public void AppendEntries(long term, long leaderId, int prevLogIndex, String[] logEntries, int leaderCommit) {
@@ -22,17 +12,8 @@ public class Leader extends Node {
     }
 
 
-    public void sendHeartBeat(int term, Address leader) {
+    public void sendHeartBeat(int term, String leader) {
 
     }
 
-    @Override
-    public Boolean receiveAppendEntries() {
-        return null;
-    }
-
-    @Override
-    public Boolean receiveRequestVote() {
-        return null;
-    }
 }
