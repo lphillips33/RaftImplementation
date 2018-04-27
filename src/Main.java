f82196874853c4a3b5cd2d52646f18c63330562d
Logan Phillips <LoganPhillips@Logans-MacBook-Pro-2.local>
2018-04-27 16:36:45 -0400

    added stuff for testing

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,19 @@ import com.reber.raft.AppendEntriesProtos.AppendEntries;
 import com.reber.raft.AppendEntriesResponseProtos.AppendEntriesResponse;
 import com.reber.raft.RequestVoteProtos.RequestVote;
 import com.reber.raft.RequestVoteResponseProtos.RequestVoteResponse;
+import sun.nio.ch.Net;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Hello World!");
+
+        Node node = new Node();
+
+        node.run();
+
+        Network network = new Network(node);
+
+        //AppendEntries appendEntries = AppendEntries.newBuilder().set
+
     }
 }
